14a46f94b5da3b61a6e5f60165f6f05ecdf15065
TonyPythoneer <tonypythoneer@gmail.com>
2017-09-11 14:45:09 +0800

    Init discord bot

---
+++ README.md
@@ -0,0 +1,11 @@
+# Seria Bot
+
+(TODO....)
+
+## Development Setup
+
+```bash
+$ (sudo) pip install pre-commit
+$ pre-commit install
+$ pre-commit autoupdate
+```
---
+++ main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import asyncio
+import os
+
+import discord
+
+DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+
+if __name__ == '__main__':
+    client.run(DISCORD_TOKEN)
