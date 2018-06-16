1c32e9dc57778864de0d50bd81fe925acf373f99
TonyPythoneer <tonypythoneer@gmail.com>
2018-06-16 16:24:50 +0800

    Build bot command

--- bot_command/__init__.py
+++ bot_command/__init__.py
@@ -1,34 +1,35 @@
 # -*- coding: utf-8 -*-
 from bot_command.roster import roster
+from bot_command.anton import anton
+from discord import Client, Message
 
 
-class BotCommand(object):
-    def __init__(self, funcs):
-        self.mapping = {func.__name__: func for func in funcs}
-
-    def process_message(self, message: str):
-        if message.startswith('seria'):
-            # full command process
-            # 'seria anton 1' -> ['anton', '1']
-            args = message.split(' ')[1:]
-        elif message.startswith('!'):
-            # short command process
-            # '!anton 1' -> ['anton', '1']
-            args = message[1:].split(' ')
-        else:
-            return
-
-        if len(args) < 1:
-            return
-
-        cmd_namespace, cmd_args = args[0], args[1:]
-        func = self.mapping.get(cmd_namespace)
-        if func is None:
-            return
-        result = func(*cmd_args)
-        return result
-
-
-bot_command = BotCommand([
+COMMAND_LIST = (
+    anton,
     roster,
-])
+)
+COMMAND_MAPPING = {func.__name__: func for func in COMMAND_LIST}
+
+
+async def process_message(client: Client, message: Message):
+    content = str(message.content).lower()
+    if content.startswith('seria'):
+        # full command process
+        # 'seria anton 1' -> ['anton', '1']
+        args = content.split(' ')[1:]
+    elif content.startswith('!'):
+        # short command process
+        # '!anton 1' -> ['anton', '1']
+        args = content[1:].split(' ')
+    else:
+        return
+
+    if len(args) < 1:
+        return
+
+    cmd_namespace, cmd_args = args[0], args[1:]
+    func = COMMAND_MAPPING.get(cmd_namespace)
+    if func is None:
+        return
+    result = await func(client, message, *cmd_args)
+    return result
